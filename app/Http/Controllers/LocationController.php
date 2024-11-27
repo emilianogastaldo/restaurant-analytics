1a1c5a79007a1f28e8617db7a1b9c3c23fe779c9
emilianogastaldo <emiliano0303@gmail.com>
2024-11-27 15:18:26 +0100

    renamed controllers

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Location extends Controller
+class LocationController extends Controller
 {
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/WeatherRecordController.php
+++ app/Http/Controllers/WeatherRecordController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WeatherRecord extends Controller
+class WeatherRecordController extends Controller
 {
     /**
      * Display a listing of the resource.
