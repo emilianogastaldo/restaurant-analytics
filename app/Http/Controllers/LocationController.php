6d38c433b8e88e1b6799f1555f4514b447684b58
emilianogastaldo <emiliano0303@gmail.com>
2024-12-01 21:49:42 +0100

    recover and save weather's data

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Location;
+use App\Models\WeatherRecord;
+use DateTime;
+use DateTimeZone;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class LocationController extends Controller
 {
@@ -39,7 +43,52 @@ class LocationController extends Controller
         // Salvo i dati
         $new_location->save();
 
-        return view('welcome');
+        $coordinates = $new_location->latitude.','.$new_location->longitude;
+        // Faccio la chiamata API per raccogliere le informazioni sul meteo della location creata
+        $res_weather = Http::withoutVerifying()->get('http://api.weatherapi.com/v1/forecast.json',
+            [
+                'key' => env('WEATHER_API_KEY'),
+                'q' => $coordinates,
+                'days' => 3
+            ]
+        );
+        $res_json = $res_weather->json();
+
+        // Destrutturo la risposta
+        ["current" => $current, "forecast" => $forecast] = $res_json;
+
+        // Creo il raccoglitore per i dati del meteo
+        $data_to_save = [];
+
+        // Registro i dati 
+        // Entro nell'array associativo forecastday
+        foreach($forecast["forecastday"] as $day){
+            // Entro nell'array associativo hour
+            foreach($day["hour"] as $hour){
+                // Salvo i dati nel raccoglitore
+                $data_to_save[] = $hour;
+            }
+        }
+
+        // Salvo i dati nel database
+        foreach($data_to_save as $day){
+            $new_record = new WeatherRecord();
+            
+            $new_record->temperature = $day["temp_c"];
+            $new_record->timestamp = $day["time"];
+            $new_record->humidity = $day["humidity"];
+            $new_record->precipitation = $day["precip_mm"];
+            $new_record->wind_speed = $day["wind_kph"];
+            $new_record->condition = $day["condition"]["text"];
+            $new_record->location_id = $new_location->id;
+
+            $new_record->save();
+        }
+
+        $dataOra = new DateTime("now", new DateTimeZone("Europe/Rome"));
+        
+        // Rindirizzo alla pagina della location creata
+        return to_route('locations.show', $new_location->id);
     }
 
     /**
